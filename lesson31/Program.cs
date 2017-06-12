6255ed635117c247ea33ef03422b38115148df19
shawnswrld <shawnswrld@gmail.com>
2017-06-12 11:42:47 -0400

    shawn1

---
+++ lesson31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
